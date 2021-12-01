0e103b6f176035968fa61e6980c92abbedc145d2
Deepak0844 <deepakram0105@gmail.com>
2021-12-01 13:31:17 +0530

    updated

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // const { query } = require('express')
-const app = express()
 import express from "express";
 import {MongoClient} from "mongodb";
 import dotenv from 'dotenv';
 import {moviesRouter} from "./routes/movies.js"
 
 dotenv.config(); //all keys it will put in process.env
-
-   const PORT=process.env.PORT; // heroku will auto assign available port
+   const app = express()
+   const PORT = process.env.PORT; // heroku will auto assign available port
 
    app.use(express.json());  //every request in the app body is parsed as json
    //express.json() - inbuilt middleware
@@ -18,7 +17,7 @@ const MONGO_URL = process.env.MONGO_URL
   async function createConnections(){
   const client = new MongoClient(MONGO_URL);
   await client.connect();  //promise
-  console.log("mongodb connected")
+  console.log("mongodb connected");
   return client;
 }
 export const client = await createConnections();
